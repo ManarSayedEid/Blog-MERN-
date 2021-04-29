e6964899f2f7a304726d5231736d421f611106d1
ManarSayedEid <manarsayed1994@gmail.com>
2021-04-29 08:41:11 +0200

    feat: user model

--- server.js
+++ server.js
@@ -5,12 +5,19 @@ const app = express();
 
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("yes");
-});
+// app.get("/", (req, res) => {
+//   res.send("yes");
+// });
+
+require("./models/User");
+
+app.use("/api/users", require("./routes/user"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/profile", require("./routes/profile"));
+app.use("/api/posts", require("./routes/post"));
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server Listening at ${PORT}`);
+  console.log(`Server Listening on port ${PORT}`);
 });
